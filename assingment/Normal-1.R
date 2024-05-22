6bf7caff1c6fb8324dcfb71fce8763c7914d3256
dchamp16 <dchamp16@gmail.com>
2024-05-22 10:21:53 -0600

    added assignment

--- assingment/Normal-1.R
+++ assingment/Normal-1.R
@@ -8,23 +8,24 @@ library(readxl)
 
 
 #### You will need to change this value for your own computer
-dataDir = "/Users/10975067/Documents/John/UVU/Classes/STAT 2040/2040 Ritems/ncha/Files"
+current_dir <- dirname(normalizePath("."))
+dataDir <- file.path(current_dir, "Github", "r-stat-2050", "datas")
 
 ### Read in the Dataset
 ncha = read_excel(paste(dataDir, 
-                "NCHA-III WEB SPRING 2021 UTAH VALLEY UNIVERSITY  - TIMESTAMP.xlsx", 
-                sep = "/"), sheet = "NCHA-III WEB SPRING 2021 UTAH V")
+                        "NCHA-III WEB SPRING 2021 UTAH VALLEY UNIVERSITY  - TIMESTAMP.xlsx", 
+                        sep = "/"), sheet = "NCHA-III WEB SPRING 2021 UTAH V")
 
 ncha = as.data.frame(ncha)
 
 #### mean
-myMean = mean(ncha$N3Q60, na.rm = TRUE)
+myMean = mean(ncha$N3Q71, na.rm = TRUE)
 
 
 #### SD
-mySD = sd(ncha$N3Q60, na.rm = TRUE)
+mySD = sd(ncha$N3Q71, na.rm = TRUE)
 
-myVal = myMean + .567*mySD
+myVal = myMean - 1.23 * mySD
 myVal
 
 pnorm(myVal, mean = myMean, sd = mySD)
